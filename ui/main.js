5068d0a1312e1d8d865fbe58514482a842381a15
im100ruv <karnasaurabh123@gmail.com>
2016-10-31 10:56:02 +0530

    changed div element using Javascript

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+
+//change the text of the mainText div
+//it changes very fast so old text is not visible
+var newText = document.getElementById('mainText');
+newText.innerHTML = 'This is the new text changed by Javascript.';
