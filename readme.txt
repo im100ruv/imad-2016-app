beb7083df72147cf2c4a5dc296fe003354dd16bd
im100ruv <karnasaurabh123@gmail.com>
2016-10-31 18:20:35 +0530

    counter button captures counter endpoint

---
+++ readme.txt
@@ -0,0 +1,14 @@
+
+This project uses gulp to automate server restart when changes are made and saved to the files, so that you don't require
+to restart server manually 
+
+
+Steps to run application
+
+1)Make sure you have node installed on your system
+2)Open command line to root of the project directory
+3)Run command :  npm install gulp -g
+4)Run command :  npm install gulp --save
+5)Run commadn :  npm install gulp-nodemon --save
+6)Run 'gulp' in node.js command prompt
+7)At browser to see home page type : http://localhost:3000/
--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,24 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById('counter');
 
-//Move the image
-var img = document.getElementById('me');
-var marginLeft = 0;
-function moveRight() {
-	marginLeft = marginLeft + 1;
-	img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-	var interval = setInterval(moveRight, 50);
-}
+button.onclick = function () {
+	//Create a request object
+	var request = new XMLHttpRequest();
+
+	//Capture the response and store it in a variable
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			//Take some action
+			if (request.status === 200) {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+		//Not done yet
+	};
+
+	//Make the request
+	request.open('GET', '/counter', true);
+	request.send(null);
+};
