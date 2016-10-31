8b773ce804ef1c6097178343de4bb38341d13e05
im100ruv <karnasaurabh123@gmail.com>
2016-10-31 11:00:06 +0530

    restored text changes

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,1 @@
 console.log('Loaded!');
-
-//change the text of the mainText div
-//it changes very fast so old text is not visible
-var newText = document.getElementById('mainText');
-newText.innerHTML = 'This is the new text changed by Javascript.';
